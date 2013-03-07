413498ad17ef57fb84415a3fe3f00ee0a6d21cea
Dan Leehr <dan.leehr@nescent.org>
2013-03-06 21:17:13 -0500

    Add confirm_logged_in to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def confirm_logged_in
+  	unless session[:user_id]
+  		flash[:notice] = "Please log in"
+  		redirect_to({:controller => 'access', :action => 'login'})
+  		return false # halts the filter
+  	else
+  		return true
+  	end
+  end
 end

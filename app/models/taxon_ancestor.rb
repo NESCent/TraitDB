4c2316bedbc6d4521729121d1a47f885fb2df829
Dan Leehr <dan.leehr@nescent.org>
2013-03-08 11:49:25 -0500

    Make parent_id and child_id assignable

--- app/models/taxon_ancestor.rb
+++ app/models/taxon_ancestor.rb
@@ -1,5 +1,5 @@
 class TaxonAncestor < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :parent_id, :child_id
   belongs_to :parent, :class_name => "Taxon"
   belongs_to :child, :class_name => "Taxon"
     

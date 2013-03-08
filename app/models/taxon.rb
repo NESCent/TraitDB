260d795d174f25acfa01d88222101477d8eae0e5
Dan Leehr <dan.leehr@nescent.org>
2013-03-08 11:48:54 -0500

    Add scopes and helpers to taxon
    Also make parent accessible

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,7 +1,8 @@
 class Taxon < ActiveRecord::Base
-  attr_accessible :name, :uri, :import_job
+  attr_accessible :name, :uri, :import_job, :parent
   belongs_to :iczn_group
   belongs_to :import_job
+  has_one :csv_dataset, :through => :import_job
   has_many :otus
 
   has_many :child_taxa, :class_name => 'TaxonAncestor', :foreign_key => 'parent_id'
@@ -10,4 +11,18 @@ class Taxon < ActiveRecord::Base
   has_one :parent_taxon, :class_name => 'TaxonAncestor', :foreign_key => 'child_id'
   has_one :parent, :through => :parent_taxon
 
+  scope :sorted, order('taxa.name ASC')
+  
+  def dataset_name
+    csv_dataset.csv_file.name if csv_dataset
+  end
+  
+  def iczn_group_name
+    iczn_group.name if iczn_group
+  end
+  
+  def parent_name
+    parent.name if parent
+  end
+  
 end

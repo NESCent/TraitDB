23b8aad3bb299284ce74f328cbb013ea24e152e5
Dan Leehr <dan.leehr@nescent.org>
2013-03-11 11:12:38 -0400

    Remove position from continuous trait

--- db/migrate/20130311140347_create_continuous_trait_values.rb
+++ db/migrate/20130311140347_create_continuous_trait_values.rb
@@ -1,7 +1,6 @@
 class CreateContinuousTraitValues < ActiveRecord::Migration
   def change
     create_table :continuous_trait_values do |t|
-      t.integer :position
       t.references :otu
       t.references :continuous_trait
       t.float

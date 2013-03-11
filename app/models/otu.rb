a06d92dcb41ef57eb77ec0a8dce9147fd3c81607
Dan Leehr <dan.leehr@nescent.org>
2013-03-11 12:22:25 -0400

    Add method to get categorical_traits from Otu

--- app/models/otu.rb
+++ app/models/otu.rb
@@ -4,11 +4,13 @@ class Otu < ActiveRecord::Base
   belongs_to :import_job
   has_one :csv_dataset, :through => :import_job
   has_many :categorical_trait_values, :dependent => :destroy
+  has_many :categorical_trait_categories, :through => :categorical_trait_values
   has_many :continuous_trait_values, :dependent => :destroy
   scope :sorted, order('name ASC')
 
-  # scope the categories
-  # scope the traits
+  def categorical_traits
+    categorical_trait_categories.map{|x| x.categorical_trait}.uniq
+  end
 
   def dataset_name
     csv_dataset.csv_file_file_name if csv_dataset

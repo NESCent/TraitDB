a6b616161849e539b3ba3ee5c2722fef2a44dd45
Dan Leehr <dan.leehr@nescent.org>
2013-03-12 13:11:21 -0400

    Add taxa scopes: species/family/order/genus/ungrouped

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,6 +1,7 @@
 class Taxon < ActiveRecord::Base
   attr_accessible :name, :uri, :import_job, :parent
   belongs_to :iczn_group
+
   belongs_to :import_job
   has_one :csv_dataset, :through => :import_job
   has_many :otus, :dependent => :destroy
@@ -12,6 +13,12 @@ class Taxon < ActiveRecord::Base
   has_one :parent, :through => :parent_taxon
 
   scope :sorted, order('name ASC')
+
+  scope :species_taxa, where(:iczn_group_id => IcznGroup.find_by_name("species"))
+  scope :family_taxa, where(:iczn_group_id => IcznGroup.find_by_name("family"))
+  scope :order_taxa, where(:iczn_group_id => IcznGroup.find_by_name("order"))
+  scope :genus_taxa, where(:iczn_group_id => IcznGroup.find_by_name("genus"))
+  scope :ungrouped_taxa, where(:iczn_group_id => nil)
 
   def dataset_name
     csv_dataset.csv_file_file_name if csv_dataset

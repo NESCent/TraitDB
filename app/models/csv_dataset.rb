6bf5906cbe193823c4ddacb2703554101caadb2d
Dan Leehr <dan.leehr@nescent.org>
2013-03-14 15:40:47 -0400

    Add task to clear database.  Add dependent destroy from dataset->import job

--- app/models/csv_dataset.rb
+++ app/models/csv_dataset.rb
@@ -3,7 +3,7 @@ class CsvDataset < ActiveRecord::Base
   has_attached_file :csv_file
   belongs_to :project
   belongs_to :user
-  has_many :import_jobs
+  has_many :import_jobs, :dependent => :destroy
   
   @@fields = ["chromosome number (female)","c-value","predicted ploidy (1n, 2n, 3n, 4n)","molecular basis (dosage, Y dominant, W dominant)","gametophytic chromosome number (minimum)","sporophytic chromosome number (mean)","Hybrid (no, yes)","molecular basis (dosage, Y dominant, W dominant, DM-W)"]
   def self.fields
---
+++ lib/tasks/traitdb.rake
@@ -0,0 +1,10 @@
+namespace :traitdb do
+  desc "Clear all imported data and taxa"
+  task :clear => :environment do
+    Taxon.destroy_all
+    CsvDataset.destroy_all
+    CategoricalTrait.destroy_all
+    ContinuousTrait.destroy_all
+  end
+
+end

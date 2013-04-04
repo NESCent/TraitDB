8a5d60213173483616d14942959744818cafe2fe
Dan Leehr <dan.leehr@nescent.org>
2013-04-04 15:18:06 -0400

    Support for downloading results in CSV
    results action responds to .csv format
    Added a results.csv.erb template
    Also added button with a link to the current results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,15 @@
 module SearchHelper
+  def params_for_search(params)
+    {:htg => params['htg'],
+     :order => params['order'],
+     :family => params['family'],
+     :genus => params['genus'],
+     :categorical_trait_name => params['categorical_trait_name'],
+     :categorical_trait_values => params['categorical_trait_values'],
+     :only_rows_with_data => params['only_rows_with_data'],
+     :continuous_trait_name => params['continuous_trait_name '],
+     :continuous_trait_value_predicates => params['continuous_trait_value_predicates'],
+     :include_references => params['include_references']
+    }
+  end
 end

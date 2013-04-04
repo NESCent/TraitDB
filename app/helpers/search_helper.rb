6fe8ac2cfd316caf5feaddfed8da2a91448e9190
Dan Leehr <dan.leehr@nescent.org>
2013-04-04 15:38:50 -0400

    Fix missing/broken continuous params in helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,8 +7,9 @@ module SearchHelper
      :categorical_trait_name => params['categorical_trait_name'],
      :categorical_trait_values => params['categorical_trait_values'],
      :only_rows_with_data => params['only_rows_with_data'],
-     :continuous_trait_name => params['continuous_trait_name '],
+     :continuous_trait_name => params['continuous_trait_name'],
      :continuous_trait_value_predicates => params['continuous_trait_value_predicates'],
+     :continuous_trait_entries => params['continuous_trait_entries'],
      :include_references => params['include_references']
     }
   end

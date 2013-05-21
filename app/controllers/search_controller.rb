636e12918514bedfcb02da238cd7a71ef6abc477
Dan Leehr <dan.leehr@nescent.org>
2013-05-21 17:22:33 -0400

    Populate search results completely so that template doesn't fetch values
    WIP, and need to rewrite CSV template

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -87,8 +87,11 @@ class SearchController < ApplicationController
       end
     end
 
+    headers = {}
     # This just gets the headers
-    continuous_traits = ContinuousTrait.where(:id => continuous_trait_predicate_map.keys)
+    headers[:continuous_traits] = ContinuousTrait.where(:id => continuous_trait_predicate_map.keys).map do |continuous_trait|
+      {:id => continuous_trait.id, :name => continuous_trait.name}
+    end
 
     # Categorical Trait Values
     categorical_trait_category_map = {} # Map of categorical_trait_ids to arrays of categorical_trait_category_ids
@@ -106,9 +109,11 @@ class SearchController < ApplicationController
       end
     end
 
-    categorical_traits = CategoricalTrait.where(:id => categorical_trait_category_map.keys)
+    headers[:categorical_traits] = CategoricalTrait.where(:id => categorical_trait_category_map.keys).map do |categorical_trait|
+      {:id => categorical_trait.id, :name => categorical_trait.name}
+    end
 
-    otus = []
+    rows = []
     # AND or OR
     trait_operator = params['trait_operator'] || OPERATORS[:or]
 
@@ -137,7 +142,11 @@ class SearchController < ApplicationController
 
       # Assemble a set of Otus for this selected row
       Otu.in_taxon(lowest_id, lowest_group).each do |otu|
+        # Start with a hash containing the OTU
+        # This row will only be included in the output set if the criteria is met
+        row = { :otu => otu, :sort_name => otu.name }
         # for each Otu in the list, see if it has the specified trait values
+        continuous_trait_values = []
         matched_trait_otu_count = 0
         continuous_trait_predicate_map.each do |trait_id, predicates_array|
           matched_values = otu.continuous_trait_values.where(:continuous_trait_id => trait_id)
@@ -148,40 +157,43 @@ class SearchController < ApplicationController
           unless matched_values.nil? || matched_values.empty?
             # Keep track of how many traits matched to determine if AND was successful
             matched_trait_otu_count += 1
+            values = matched_values.map{|continuous_trait_value| continuous_trait_value.value}
+            continuous_trait_values << {:continuous_trait_id => trait_id, :values => values }
           end
         end
+        row[:continuous_trait_values] = continuous_trait_values # may be an empty array
 
+        categorical_trait_values = []
         total_queried_categorical_values = 0
         categorical_trait_category_map.each do |trait_id, category_ids|
           total_queried_categorical_values += category_ids.count
           matched_values = otu.categorical_trait_values.where(:categorical_trait_id => trait_id).where(:categorical_trait_category_id => category_ids)
           unless matched_values.nil? || matched_values.empty?
             matched_trait_otu_count += matched_values.count
+            values = matched_values.map{|categorical_trait_value| categorical_trait_value.categorical_trait_category.name }
+            categorical_trait_values << {:categorical_trait_id => trait_id, :values => values }
           end
         end
-
-        # for the case of AND, matched_trait_otu_count must equal continuous_trait_predicate_map.length
+        row[:categorical_trait_values] = categorical_trait_values # may be an empty array
+        # for the case of AND, we must have the same number of results as our input criteria before we add the row to our output set
         if trait_operator == OPERATORS[:and]
           if matched_trait_otu_count == (continuous_trait_predicate_map.length + total_queried_categorical_values)
-            otus << otu
+            rows << row
           end
         else
           # OR, just make sure matched_trait_otu_count > 0
-          if matched_trait_otu_count > 0
-            otus << otu
+          if matched_trait_otu_count > 0 || params['only_rows_with_data'].nil?
+            rows << row
           end
         end
-        # TODO: inject trait values and headers into the output dataset, instead of the template doing this
       end
     end
-    otus.sort!
+    rows.sort! {|a,b| a[:sort_name] <=> b[:sort_name]}
 
     # data to return to view
-    @results = {}
-    @results[:include_references] = !params['include_references'].nil?
-    @results[:otus] = otus
-    @results[:categorical_traits] = categorical_traits
-    @results[:continuous_traits] = continuous_traits
+    @results = {:headers => headers, # headers is a hash with keys :categorical_traits and :continuous_traits
+                :rows => rows, # rows is an array of hashes.  Each hash has :otu, :categorical_trait_values, and :continuous_trait_values
+                :include_references => !params['include_references'].nil? }
 
     respond_to do |format|
       format.csv do

970a2510784fa3980f747f33e9b53601f19d5753
Dan Leehr <dan.leehr@nescent.org>
2013-05-22 14:55:42 -0400

    Finish refactoring to support AND/OR

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -172,51 +172,65 @@ class SearchController < ApplicationController
         # Start with a hash containing the OTU
         # This row will only be included in the output set if the criteria is met
         row = { :otu => otu, :sort_name => otu.name }
+        match_map = {:continuous => [], :categorical => []}
         # for each Otu in the list, see if it has the specified trait values
         continuous_trait_values = []
-        matched_trait_otu_count = 0
         continuous_trait_predicate_map.each do |trait_id, predicate_array|
           matched_values = otu.continuous_trait_values.where(:continuous_trait_id => trait_id)
+          coded_count = matched_values.count
           # predicate_array is an object ready to be dropped into a where() call.
           # The operator (and/or) has already been added to the query
           matched_values = matched_values.where(predicate_array)
-
-          # If this OTU had a trait value that met the criteria, increment our counter
-          unless matched_values.nil? || matched_values.empty?
-            # Keep track of how many traits matched to determine if AND was successful
-            matched_trait_otu_count += 1
+          matched_count = matched_values.count
+          unless matched_values.empty?
             values = matched_values.map{|continuous_trait_value| continuous_trait_value.value}
             continuous_trait_values << {:continuous_trait_id => trait_id, :values => values }
           end
+          # requested count is 1 because predicates for continuous are either met or not met
+          match_map[:continuous] << {:trait_id => trait_id, :coded_count => coded_count,
+                                     :requested_count => 1,
+                                     :matched_count => matched_count}
         end
         row[:continuous_trait_values] = continuous_trait_values # may be an empty array
 
         categorical_trait_values = []
-        total_queried_categorical_values = 0
         categorical_trait_category_map.each do |trait_id, category_ids|
-          total_queried_categorical_values += category_ids.count
+          requested_count = category_ids.count
           matched_values = otu.categorical_trait_values.where(:categorical_trait_id => trait_id)
+          coded_count = matched_values.count
           # category_ids is an array of the category values selected on the form.
           # If it is empty, then the user did not specify any category IDs to filter on, so include everything
           unless category_ids.empty?
             matched_values = matched_values.where(:categorical_trait_category_id => category_ids)
           end
-
-          unless matched_values.nil? || matched_values.empty?
-            matched_trait_otu_count += matched_values.count
+          matched_count = matched_values.count
+          unless matched_values.empty?
             values = matched_values.map{|categorical_trait_value| categorical_trait_value.categorical_trait_category.name }
             categorical_trait_values << {:categorical_trait_id => trait_id, :values => values }
           end
+          match_map[:categorical] << {:trait_id => trait_id, :coded_count => coded_count,
+                                      :requested_count => requested_count,
+                                      :matched_count => matched_count}
         end
         row[:categorical_trait_values] = categorical_trait_values # may be an empty array
-        # for the case of AND, we must have the same number of results as our input criteria before we add the row to our output set
+
+        # for the case of AND, make sure the matched count is >= the coded count
+        # Only the sums of these are used, so the intermediate hash is unnecessary
+        total_categorical_requested = match_map[:categorical].map{|t| t[:requested_count]}.reduce(:+) || 0
+        total_categorical_matched = match_map[:categorical].map{|t| t[:matched_count]}.reduce(:+) || 0
+        total_categorical_coded = match_map[:categorical].map{|t| t[:coded_count]}.reduce(:+) || 0
+        total_continuous_requested = match_map[:continuous].map{|t| t[:requested_count]}.reduce(:+) || 0
+        total_continuous_matched = match_map[:continuous].map{|t| t[:matched_count]}.reduce(:+) || 0
+        total_continuous_coded = match_map[:continuous].map{|t| t[:coded_count]}.reduce(:+) || 0
+
         if trait_operator == OPERATORS[:and]
-          if matched_trait_otu_count == (continuous_trait_predicate_map.length + total_queried_categorical_values)
+          # For AND, include this row only if the total number of trait filters requested meets the number of trait values found
+          if total_categorical_matched == total_categorical_requested && total_continuous_matched >= total_continuous_requested
             rows << row
           end
         else
-          # OR, just make sure matched_trait_otu_count > 0
-          if matched_trait_otu_count > 0 || params['only_rows_with_data'].nil?
+          # For OR, include if any values matched or if include all data was selected but nothing was coded
+          if total_categorical_matched > 0 || total_continuous_matched > 0 || (params['only_rows_with_data'].nil? && total_categorical_coded == 0 && total_continuous_coded == 0)
             rows << row
           end
         end
@@ -245,165 +259,6 @@ class SearchController < ApplicationController
       end
       format.html
     end
-  end
-
-
-  def results2
-    # Rows are determined by selected taxa
-    # params = {'htg' => {'0' => '21043', '2' => ''}}
-
-    # Collect the IDs of the selected taxa
-    otus = []
-
-    params['htg'].reject{|k,v| v.empty?}.each do |k,v|
-      # Start with the higher group and narrow if specified
-      lowest_id =  Integer(v)
-      lowest_group = 'htg'
-      if params['order'] && params['order'][k]
-        unless params['order'][k].blank?
-          lowest_id = Integer(params['order'][k])
-          lowest_group = 'order'
-        end
-      end
-      if params['family'] && params['family'][k]
-        unless params['family'][k].blank?
-          lowest_id = Integer(params['family'][k])
-          lowest_group = 'family'
-        end
-      end
-      if params['genus'] && params['genus'][k]
-        unless params['genus'][k].blank?
-          lowest_id = Integer(params['genus'][k])
-          lowest_group = 'genus'
-        end
-      end
-
-      # Find all OTUs with the corresponding level
-      otus += Otu.in_taxon(lowest_id, lowest_group)
-    end
-
-    # sort the otus
-    # Sorting by sort_name takes a long time, leaving this to sort by id!
-    otus.sort!
-
-    # Traits - become columns in output
-
-    # If a value is selected for the trait, filter the OTUs to the matching rows
-    # But keep in mind that multiple values may be selected here, so we need to collect them all
-    categorical_trait_value_map = {}
-    if params['categorical_trait_name']
-        params['categorical_trait_name'].reject{|k,v| v.empty?}.each do |k,v|
-        trait_id = Integer(v)
-        trait_category_ids = categorical_trait_value_map[trait_id] || []
-        if params['categorical_trait_values'][k]
-          unless params['categorical_trait_values'][k].blank?
-            trait_category_ids << Integer(params['categorical_trait_values'][k])
-          end
-        end
-        categorical_trait_value_map[trait_id] = trait_category_ids
-      end
-    end
-
-    categorical_traits = CategoricalTrait.where(:id => categorical_trait_value_map.keys)
-
-    # If only_rows_with_data was checked, remove OTUs that were not coded for the trait
-    if (categorical_traits.count > 0) && params['only_rows_with_data']
-      otus.reject! {|otu| (otu.categorical_traits & categorical_traits).empty? }
-    end
-
-    # Filter out OTUs that were not coded at all if a trait value was chosen
-    unless categorical_trait_value_map.values.flatten.empty?
-      # trait values were selected, OTUs that don't have them.
-      otus.reject! do |otu|
-        remove = true
-        categorical_trait_value_map.each do |trait_id, trait_value_id|
-          leftovers = otu.categorical_trait_categories.map{|c| c.id} - trait_value_id
-          if leftovers.size < otu.categorical_trait_categories.size
-            # keep this otu since it has a coding that matches the filter
-            remove = false
-          end
-        end
-        remove
-      end
-    end
-
-    # Continuous Trait Values
-    continuous_trait_predicate_map = {}
-    if params['continuous_trait_name']
-      params['continuous_trait_name'].reject{|k,v| v.empty?}.each do |k,v|
-        trait_id = Integer(v)
-        trait_value_ids = continuous_trait_predicate_map[trait_id] || []
-
-        # Check for the equals/less than/etc
-        # get the predicate for this row
-        if params['continuous_trait_value_predicates'][k] && params['continuous_trait_entries'][k]
-          unless params['continuous_trait_entries'][k].blank?
-            field_value = Float(params['continuous_trait_entries'][k])
-            case params['continuous_trait_value_predicates'][k]
-              when 'gt'
-                trait_value_ids << ['value > ?', field_value]
-              when 'lt'
-                trait_value_ids << ['value < ?', field_value]
-              when 'eq'
-                trait_value_ids << ['value = ?', field_value]
-              when 'ne'
-                trait_value_ids << ['value != ?', field_value]
-            end
-          end
-        end
-        continuous_trait_predicate_map[trait_id] = trait_value_ids
-      end
-    end
-
-    # This just gets the headers
-    continuous_traits = ContinuousTrait.where(:id => continuous_trait_predicate_map.keys)
-
-    # If only_rows_with_data was checked, remove OTUs that were not coded for the trait
-    if (continuous_traits.count > 0) && params['only_rows_with_data']
-      otus.reject! {|otu| (otu.continuous_traits & continuous_traits).empty? }
-    end
-
-    # Filter out OTUs that were not coded at all if a trait value was chosen
-    unless continuous_trait_predicate_map.values.flatten.empty?
-      # At least one predicate was chosen, filter out OTUs that don't match
-      otus.reject! do |otu|
-        remove = true
-        matched_values = []
-        continuous_trait_predicate_map.each do |trait_id, predicates_array|
-          matched_values = otu.continuous_trait_values.where(:continuous_trait_id => trait_id)
-          predicates_array.each do |predicate|
-            matched_values = matched_values.where(predicate)
-          end
-        end
-        # remove if the otu didn't match any values
-        matched_values.empty?
-      end
-    end
-
-    # data to return to view
-    @results = {}
-    @results[:include_references] = !params['include_references'].nil?
-    @results[:otus] = otus
-    @results[:categorical_traits] = categorical_traits
-    @results[:continuous_traits] = continuous_traits
-
-    respond_to do |format|
-      format.csv do
-        filename = "results-#{Time.now.strftime("%Y%m%d")}.csv"
-        if request.env['HTTP_USER_AGENT'] =~ /msie/i
-          headers['Pragma'] = 'public'
-          headers["Content-type"] = "text/plain"
-          headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
-          headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
-          headers['Expires'] = "0"
-        else
-          headers["Content-Type"] ||= 'text/csv'
-          headers["Content-Disposition"] = "attachment; filename=\"#{filename}\""
-        end
-      end
-      format.html
-    end
-
   end
 
   private

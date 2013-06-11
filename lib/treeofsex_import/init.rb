07b13f2e7d560ba0a70bc6ad33f5f80dd999f6b2
Dan Leehr <dan.leehr@nescent.org>
2013-06-11 10:11:09 -0400

    Working around in command-line validator to harness new template

--- lib/treeofsex_import/init.rb
+++ lib/treeofsex_import/init.rb
@@ -1,24 +1,33 @@
 #!/usr/bin/env ruby
 
-#### Tree of Sex CSV File validator ####
+#### TraitDB YML/CSV File validator ####
 
 APP_ROOT = File.dirname(__FILE__) # a constant, based on the directory containing this file
 
 $:.unshift APP_ROOT # puts the directory into the search path
 
-require 'validator' # loads once the validator.rb file
-validator = TreeOfSexImport::Validator.new(ARGV[0])
-validator.quantitative_header_start = "Chromosome number (female)"
-validator.quantitative_header_end = "c-value"
-validator.qualitative_header_start = "Predicted ploidy (1,2,3,4)"
-validator.qualitative_header_end = "Molecular basis (dosage,Y dominant,W dominant)"
+# require 'validator' # loads once the validator.rb file
+require 'import_template'
 
-results = validator.validate
-puts results
-results = validator.parse
+# Instantiate a template validator with the first argument
+# which should be a YML file
 
-results[:issues].each do |issue|
-  puts issue
-end
+import_template = TraitDB::ImportTemplate.new(ARGV[0])
+puts import_template.template
 
-puts results[:issues].count
+# 
+# validator = TreeOfSexImport::Validator.new(ARGV[0])
+# validator.quantitative_header_start = "Chromosome number (female)"
+# validator.quantitative_header_end = "c-value"
+# validator.qualitative_header_start = "Predicted ploidy (1,2,3,4)"
+# validator.qualitative_header_end = "Molecular basis (dosage,Y dominant,W dominant)"
+# 
+# results = validator.validate
+# puts results
+# results = validator.parse
+# 
+# results[:issues].each do |issue|
+#   puts issue
+# end
+# 
+# puts results[:issues].count

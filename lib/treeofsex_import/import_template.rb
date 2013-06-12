79c6f1de50610dcac37a54492e872387924ab70d
Dan Leehr <dan.leehr@nescent.org>
2013-06-12 14:33:19 -0400

    Refactoring in import_template

--- lib/treeofsex_import/import_template.rb
+++ lib/treeofsex_import/import_template.rb
@@ -12,34 +12,31 @@ module TraitDB
       end
     end
     
+    # template name
     def name
       @template['template_name']
     end
     
+    # taxonomy
     def taxonomy_columns # a hash that maps constants to the CSV column names, reverse it to get a map the other way
       @template['taxonomy_columns']
     end
 
+    # metadata
     def metadata_columns # a hash that maps constants to the CSV column names, reverse it to get a map the other way
       @template['metadata_columns']
     end
 
+    # options
     def trait_options # a hash that includes things like source_prefix, require_source, and notes_prefix
       @template['trait_options']
     end
     
-    def trait_groups
-      @template['trait_groups']
-    end
-    
+    # trait names
     def categorical_trait_names
       @template['categorical_trait_columns'].map{|x| x['name'] }
     end
-    
-    def categorical_trait_values(trait_name)
-      @template['categorical_trait_columns'].find{|x| x['name'] == trait_name}['values']
-    end
-    
+
     def continuous_trait_names
       @template['continuous_trait_columns'].map{|x| x['name'] }
     end
@@ -51,16 +48,43 @@ module TraitDB
     def continuous_trait_names_in_group(group_name)
       @template['continuous_trait_columns'].select{|x| x['groups'].include? group_name}.map{|x| x['name']}
     end
+
+    # trait values
+    def categorical_trait_values(trait_name)
+      categorical(trait_name)['values']
+    end
+    
+    # groups
+    def trait_groups
+      @template['trait_groups']
+    end
     
     def groups_for_categorical_trait(trait_name)
-      @template['categorical_trait_columns'].find{|x| x['name'] == trait_name}['groups']
+      categorical(trait_name)['groups']
     end
     
     def groups_for_continuous_trait(trait_name)
-      @template['continuous_trait_columns'].find{|x| x['name'] == trait_name}['groups']
+      continuous(trait_name)['groups']
+    end
+    
+    # formats
+    def continuous_trait_format(trait_name)
+      continuous(trait_name)['format']
+    end
+
+    def categorical_trait_format(trait_name)
+      categorical(trait_name)['format']
     end
 
     private
+    
+    def continuous(trait_name)
+      @template['continuous_trait_columns'].find{|x| x['name'] == trait_name}
+    end
+    
+    def categorical(trait_name)
+      @template['categorical_trait_columns'].find{|x| x['name'] == trait_name}
+    end
 
     def file_usable?
       return false unless @template_file

425c03f7c7c6f65e42fde40653ff5f570e46410a
Dan Leehr <dan.leehr@nescent.org>
2013-07-03 11:12:00 -0400

    Refactor assembly of lowest selected taxa into private method
    Also moved creation of @results hash to early in the results method so
    it is available to these private methods

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -44,18 +44,19 @@ class SearchController < ApplicationController
     unless OPERATORS.values.include? @trait_operator
       @trait_operator = OPERATORS[:or]
     end
+    @results = {}
 
-    categorical_trait_category_map, continuous_trait_predicate_map  = assemble_trait_filters(params)
+    assemble_trait_filters # populates @categorical_trait_category_map and @continuous_trait_predicate_map
 
-    columns = {}
-    # This just gets the columns
-    columns[:continuous_traits] = ContinuousTrait.where(:id => continuous_trait_predicate_map.keys).map do |continuous_trait|
+    @results[:columns] = {} # start with an empty hash for output display columns
+
+    # Output columns should include chosen continuous traits
+    @results[:columns][:continuous_traits] = ContinuousTrait.where(:id => @continuous_trait_predicate_map.keys).map do |continuous_trait|
       {:id => continuous_trait.id, :name => continuous_trait.name}
     end
 
-
-
-    columns[:categorical_traits] = CategoricalTrait.where(:id => categorical_trait_category_map.keys).map do |categorical_trait|
+    # output columns should include chosen categorical traits
+    @results[:columns][:categorical_traits] = CategoricalTrait.where(:id => @categorical_trait_category_map.keys).map do |categorical_trait|
       {:id => categorical_trait.id, :name => categorical_trait.name}
     end
 
@@ -68,40 +69,10 @@ class SearchController < ApplicationController
 
     rows = []
 
-    # The requested taxon filters have parameter names that correspond to IcznGroup names.
-    # The values are the Taxon IDs
-    sorted_groups_requested = IcznGroup.sorted.where(:name => params.keys)
-    valid_group_names = IcznGroup.sorted.map{|g| g.name}
-    columns[:iczn_groups] = sorted_groups_requested.map{|group| {:name => group.name, :id => group.id}}
-
-    # An example params hash looks like this
-    # "{"htg"=>{"0"=>"2601", "2"=>"2601"}, "order"=>{"0"=>"2602", "2"=>"2602"}, "family"=>{"0"=>"2603", "2"=>"2607"}, "genus"=>{"0"=>"2625", "2"=>"2612"}, "species"=>{"0"=>"2623", "2"=>"2613"}, "trait_types"=>{"0"=>""}, "categorical_trait_name"=>{"0"=>""}, "categorical_trait_values"=>{"0"=>""}, "continuous_trait_name"=>{"0"=>""}, "continuous_trait_value_predicates"=>{"0"=>""}, "continuous_trait_entries"=>{"0"=>""}, "include_references"=>"on", "controller"=>"search", "action"=>"results"}"
-
-    indices = []
-
-    params.each do |pk, pv|
-      next unless valid_group_names.include? pk
-      # pk is one of 'htg','order',etc...
-      # pv looks like {"0"=>"2602", "2"=>"2607"}
-      # "0" and "2" are integers in string format, and these indices may not be consecutive
-      indices += pv.keys.map{|n| n.to_i}
-    end
-    indices.sort!.uniq!
-
-    lowest_requested_taxa = []
-    indices.each do |index|
-      lowest_requested_taxon = nil
-      sorted_groups_requested.each do |group|
-        taxon_id_str = params[group.name][index.to_s]
-        unless taxon_id_str.nil? || taxon_id_str.empty?
-          lowest_requested_taxon = Taxon.find(taxon_id_str.to_i)
-        end
-      end
-      lowest_requested_taxa << lowest_requested_taxon if lowest_requested_taxon
-    end
+    analyze_lowest_requested_taxa # populates @lowest_requested_taxa, @selected_taxon_ids, and @results[:columns][:iczn_groups]
 
     # At this point, we'll have a list of the most specific taxa requested at each level
-    lowest_requested_taxa.each do |lowest_requested_taxon|
+    @lowest_requested_taxa.each do |lowest_requested_taxon|
       lowest_requested_taxon.otus.each do |otu|
         # Start with a hash containing the OTU
         # This row will only be included in the output set if the criteria is met
@@ -109,7 +80,7 @@ class SearchController < ApplicationController
         match_map = {:continuous => [], :categorical => []}
         # for each Otu in the list, see if it has the specified trait values
         continuous_trait_values = []
-        continuous_trait_predicate_map.each do |trait_id, predicate_array|
+        @continuous_trait_predicate_map.each do |trait_id, predicate_array|
           matched_values = otu.continuous_trait_values.where(:continuous_trait_id => trait_id)
           coded_count = matched_values.count
           # predicate_array is an object ready to be dropped into a where() call.
@@ -135,7 +106,7 @@ class SearchController < ApplicationController
         row[:continuous_trait_values] = continuous_trait_values # may be an empty array
 
         categorical_trait_values = []
-        categorical_trait_category_map.each do |trait_id, category_ids|
+        @categorical_trait_category_map.each do |trait_id, category_ids|
           requested_count = category_ids.count
           matched_values = otu.categorical_trait_values.where(:categorical_trait_id => trait_id)
           coded_count = matched_values.count
@@ -193,14 +164,14 @@ class SearchController < ApplicationController
     end
     rows.sort! {|a,b| a[:sort_name] <=> b[:sort_name]}
 
-    columns[:categorical_trait_notes_ids] = categorical_trait_notes_ids
-    columns[:continuous_trait_notes_ids] = continuous_trait_notes_ids
-    columns[:otu_metadata_field_names] = otu_metadata_field_names
+    @results[:columns][:categorical_trait_notes_ids] = categorical_trait_notes_ids
+    @results[:columns][:continuous_trait_notes_ids] = continuous_trait_notes_ids
+    @results[:columns][:otu_metadata_field_names] = otu_metadata_field_names
 
     # data to return to view
-    @results = {:columns => columns, # columns is a hash with keys :categorical_traits and :continuous_traits
-                :rows => rows, # rows is an array of hashes.  Each hash has :otu, :categorical_trait_values, and :continuous_trait_values
-                :include_references => !params['include_references'].nil? }
+    # @results[:columns] is a hash with keys :categorical_traits and :continuous_traits
+    @results[:rows] = rows # rows is an array of hashes.  Each hash has :otu, :categorical_trait_values, and :continuous_trait_values
+    @results[:include_references] = !params['include_references'].nil?
 
     respond_to do |format|
       format.csv do
@@ -227,9 +198,57 @@ class SearchController < ApplicationController
 
   private
 
+  # Extracts the taxon ids and the lowest requested taxa from the taxonomy portion of the search form
+  def analyze_lowest_requested_taxa
+    # The requested taxon filters have parameter names that correspond to IcznGroup names.
+    # The values are the Taxon IDs
+    sorted_groups_requested = IcznGroup.sorted.where(:name => params.keys)
+    valid_group_names = IcznGroup.sorted.pluck(:name)
+    @results[:columns][:iczn_groups]= sorted_groups_requested.map{|group| {:name => group.name, :id => group.id}}
+
+    # An example params hash looks like this
+    # "{"htg"=>{"0"=>"2601", "2"=>"2601"}, "order"=>{"0"=>"2602", "2"=>"2602"}, "family"=>{"0"=>"2603", "2"=>"2607"}, "genus"=>{"0"=>"2625", "2"=>"2612"}, "species"=>{"0"=>"2623", "2"=>"2613"}, "trait_types"=>{"0"=>""}, "categorical_trait_name"=>{"0"=>""}, "categorical_trait_values"=>{"0"=>""}, "continuous_trait_name"=>{"0"=>""}, "continuous_trait_value_predicates"=>{"0"=>""}, "continuous_trait_entries"=>{"0"=>""}, "include_references"=>"on", "controller"=>"search", "action"=>"results"}"
+
+    indices = []
+
+    params.each do |pk, pv|
+      next unless valid_group_names.include? pk
+      # pk is one of 'htg','order',etc...
+      # pv looks like {"0"=>"2602", "2"=>"2607"}
+      # "0" and "2" are integers in string format, and these indices may not be consecutive
+      indices += pv.keys.map{|n| n.to_i}
+    end
+    indices.sort!.uniq!
+
+    @lowest_requested_taxa = []
+    @selected_taxon_ids = []
+
+    indices.each do |index|
+      lowest_requested_taxon = nil
+      sorted_groups_requested.each do |group|
+        taxon_id_str = params[group.name][index.to_s]
+        unless taxon_id_str.nil? || taxon_id_str.empty?
+          @selected_taxon_ids << taxon_id_str.to_i
+          lowest_requested_taxon = Taxon.find(taxon_id_str.to_i)
+        end
+      end
+      @lowest_requested_taxa << lowest_requested_taxon if lowest_requested_taxon
+    end
+  end
+
   # extracting functionality out of search to deliver a list of traits
-  def assemble_trait_filters(params)
+  def assemble_trait_filters
     continuous_trait_filters = {}
+
+    # if All traits specified, find them all!
+    if params['select_all_traits'] # Checkbox, only exists in params if checked
+      # Need the selected taxon_ids
+      # First get all the traits from the selected taxonomy
+
+      # Then add them to the list
+    else
+
+    end
 
     # Continuous Trait Values
     if params['continuous_trait_name']
@@ -273,27 +292,26 @@ class SearchController < ApplicationController
     # The argument to a where() call should look like this: where(['value > ? AND value < ?', 1, 2])
     # joining the predicates provides the 'value > ? AND value < ?'
     # The * in front of the values array converts it to varargs
-    continuous_trait_predicate_map = {}
+    @continuous_trait_predicate_map = {}
     continuous_trait_filters.each do |trait_id, filter|
-      continuous_trait_predicate_map[trait_id] = [filter[:predicates].join(" #{@trait_operator} "), *filter[:values]]
+      @continuous_trait_predicate_map[trait_id] = [filter[:predicates].join(" #{@trait_operator} "), *filter[:values]]
     end
 
     # Categorical Trait Values
-    categorical_trait_category_map = {} # Map of categorical_trait_ids to arrays of categorical_trait_category_ids
+    @categorical_trait_category_map = {} # Map of categorical_trait_ids to arrays of categorical_trait_category_ids
     if params['categorical_trait_name']
       params['categorical_trait_name'].reject{|k,v| v.empty?}.each do |k,v|
         trait_id = Integer(v)
-        trait_category_ids = categorical_trait_category_map[trait_id] || []
+        trait_category_ids = @categorical_trait_category_map[trait_id] || []
 
         if params['categorical_trait_values'][k]
           unless params['categorical_trait_values'][k].blank?
             trait_category_ids << Integer(params['categorical_trait_values'][k])
           end
         end
-        categorical_trait_category_map[trait_id] = trait_category_ids
+        @categorical_trait_category_map[trait_id] = trait_category_ids
       end
     end
-    return [categorical_trait_category_map, continuous_trait_predicate_map]
   end
 
   def taxa_in_iczn_group_with_parents(iczn_group_id, parent_taxon_ids)

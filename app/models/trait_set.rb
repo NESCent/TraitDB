92988188d49c970b11b776d9e11e303af94823bd
Dan Leehr <dan.leehr@nescent.org>
2013-07-23 16:55:44 -0400

    Improve level funcionality in trait_sets
    Populate level from depth in path when creating TraitSet
    Add scopes for sorted and by_level
    Add class method to get all levels from a relation

--- app/models/trait_set.rb
+++ app/models/trait_set.rb
@@ -6,15 +6,24 @@ class TraitSet < ActiveRecord::Base
   has_many :continuous_traits
   attr_accessible :level, :name
   scope :by_project, lambda{|p| where(:project_id => p) unless p.nil?}
+  scope :by_level, lambda{|l| where(:level => l) unless l.nil?}
+  scope :sorted, order('name ASC')
+
+  # works on a relation/scope
+  def self.levels
+    order('level').pluck(:level).uniq
+  end
 
   # given a path (an array of strings), look for nested trait sets with the provided names
   # Create any missing nodes and return the tip
   def self.find_or_create_with_path(containing_project, path)
     last_set = nil
-    path.each do |pathname|
+    path.each_with_index do |pathname, i|
       h = {:name => pathname}
       h[:parent_trait_set_id] = last_set.id if last_set
-      last_set = containing_project.trait_sets.where(h).first_or_create
+      last_set = containing_project.trait_sets.where(h).first_or_create do |s|
+        s.level = i
+      end
     end
     last_set
   end

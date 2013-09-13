a2bd8e12dc9f06562e4c2e67953cdd3f2ce8cf9f
Dan Leehr <dan.leehr@nescent.org>
2013-09-13 14:12:20 -0400

    Added categorical trait search
    matches value for categorical trait values doesn't appear correct yet

--- app/helpers/profiling_helper.rb
+++ app/helpers/profiling_helper.rb
@@ -3,7 +3,8 @@ module ProfilingHelper
     def search_test(taxon)
       # in the production code, rows is an array.
       # what if it's a hash indexed by otu id
-      @continuous_trait_predicate_map = {815 => [], 813 => [], 814 => [], 816 => []}
+      @continuous_trait_predicate_map = Hash[[813, 814, 815, 816].map{|v| [v, [] ]}]
+      @categorical_trait_category_map = Hash[[136, 137, 138, 139, 140, 141].map{|v| [v, []]}]
       include_references = true
 
       # Need to build a matrix of OTU x TRAIT x VALUE x NOTE
@@ -15,10 +16,6 @@ module ProfilingHelper
       ContinuousTraitValue.where(:otu_id => otu_ids).where(:continuous_trait_id => @continuous_trait_predicate_map.keys).includes(:otu, :continuous_trait, :source_reference).each do |continuous_trait_value|
         trait_id = continuous_trait_value.continuous_trait_id
         # loops over the continuous_trait_value
-        puts continuous_trait_value.continuous_trait.name
-        puts continuous_trait_value.formatted_value
-
-        # continuous_trait_values << {:continuous_trait_id => trait_id, :values => values, :sources => sources, :notes => notes}
 
         # Insert a value for this OTU in the rows hash if it's empty
         # Lots of building empty hashes/arrays the first time through
@@ -26,7 +23,6 @@ module ProfilingHelper
         result_row = rows[continuous_trait_value.otu_id] ||= {}
         result_hash = result_row[:continuous] ||= {}
         # matches will be used to indicate whether or not the criteria was met
-        # TODO: check if include sources
         # There may be more than one trait value, and each could have a source and notes
         result_arrays = result_hash[trait_id] ||= {
           :values => [],
@@ -42,7 +38,35 @@ module ProfilingHelper
         predicate = @continuous_trait_predicate_map[trait_id]
         # Record a match if no predicate or if the predicate was matched
         # Note that the database will not be queried again unless there is a predicate
-        result_arrays[:matches] << predicate.length == 0 || ContinuousTraitValue.where(:id => continuous_trait_value.id).where(predicate).count == 1
+        result_arrays[:matches] << predicate.length == 0 || ContinuousTraitValue.where(:id => continuous_trait_value.id).where(predicate).count > 0
+        # TODO: How to Implement match map?
+      end
+      CategoricalTraitValue.where(:otu_id => otu_ids).where(:categorical_trait_id => @categorical_trait_category_map.keys).includes(:otu, :categorical_trait, :categorical_trait_category, :source_reference).each do |categorical_trait_value|
+        trait_id = categorical_trait_value.categorical_trait_id
+        # loops over the categorical_trait_value
+
+        # Insert a value for this OTU in the rows hash if it's empty
+        # Lots of building empty hashes/arrays the first time through
+        # would be good to abstract this out
+        result_row = rows[categorical_trait_value.otu_id] ||= {}
+        result_hash = result_row[:categorical] ||= {}
+        # matches will be used to indicate whether or not the criteria was met
+        # There may be more than one trait value, and each could have a source and notes
+        result_arrays = result_hash[trait_id] ||= {
+          :values => [],
+          :sources => [],
+          :notes => [],
+          :matches => []
+        }
+        result_arrays[:values] << categorical_trait_value.formatted_value
+        result_arrays[:sources] << categorical_trait_value.source_reference.to_s if include_references
+        notes = categorical_trait_value.otu.categorical_trait_notes(trait_id)
+        result_arrays[:notes] << notes if notes
+
+        category_ids = @categorical_trait_category_map[trait_id]
+        # Record a match if no predicate or if the predicate was matched
+        # Note that the database will not be queried again unless there is a predicate
+        result_arrays[:matches] << category_ids.length == 0 || CategoricalTraitValue.where(:id => categorical_trait_value.id).where(:categorical_trait_category_id => category_ids).count > 0
         # TODO: How to Implement match map?
       end
       # Cleanup the rows hash - remove anything with no data

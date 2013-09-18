610aa718a8a5c5827a0c0c517847826c6b65a0d6
Dan Leehr <dan.leehr@nescent.org>
2013-09-18 13:50:19 -0400

    Optimize notes fetching to avoid extra queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -120,7 +120,7 @@ class SearchController < ApplicationController
         # Notes are optional and only stored once per OTUxTrait
         # When a note is found, we record the trait_id so that the view can display a notes column for the trait
         # Notes are expected to be rare, so we only display the column if we have data
-        note = continuous_trait_value.otu.continuous_trait_notes.by_continuous_trait(trait_id).first
+        note = continuous_trait_value.otu.continuous_trait_notes.find{|n| n.continuous_trait.id == trait_id }
         if note
           continuous_trait_notes_ids << trait_id
           result_arrays[:notes] ||= note.notes
@@ -158,7 +158,7 @@ class SearchController < ApplicationController
         # Notes are optional and only stored once per OTUxTrait
         # When a note is found, we record the trait_id so that the view can display a notes column for the trait
         # Notes are expected to be rare, so we only display the column if we have data
-        note = categorical_trait_value.otu.categorical_trait_notes.by_categorical_trait(trait_id).first
+        note = categorical_trait_value.otu.categorical_trait_notes.find{|n| n.continuous_trait.id == trait_id }
         if note
           categorical_trait_notes_ids << trait_id
           result_arrays[:notes] ||= note.notes

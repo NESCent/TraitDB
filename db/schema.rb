6fb3145cadd80fb262934b70d97830b3bfde712e
Dan Leehr <dan.leehr@nescent.org>
2013-12-12 15:09:05 -0500

    Increase precision and scale of Continuous Trait Values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010153245) do
+ActiveRecord::Schema.define(version: 20131212195901) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categorical_trait_categories", force: true do |t|
     t.string   "name"
@@ -89,7 +92,7 @@ ActiveRecord::Schema.define(version: 20131010153245) do
     t.integer  "continuous_trait_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "value",               precision: 10, scale: 2
+    t.decimal  "value",               precision: 20, scale: 3
     t.integer  "source_reference_id"
   end
 

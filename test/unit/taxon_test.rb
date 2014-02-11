c011e353527193220433f94667eece8a30d94f6d
Dan Leehr <dan.leehr@nescent.org>
2014-02-11 17:06:05 -0500

    Add test case for taxa in same project as parent

--- test/unit/taxon_test.rb
+++ test/unit/taxon_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TaxonTest < ActiveSupport::TestCase
+  fixtures :taxa, :projects, :iczn_groups, :import_jobs
   # test "the truth" do
   #   assert true
   # end
+
+  test "Child taxa are in same project as parent" do
+    import_job = import_jobs(:one)
+    parent = taxa(:two)
+    child = Taxon.find_or_create_with_parent("child", parent, iczn_groups(:one), import_jobs(:one))
+    assert_equal(child.project, import_job.project)
+  end
 end

44830bdc897b38e546ed3da9818958f0693ef27d
Dan Leehr <dan.leehr@nescent.org>
2014-04-09 09:46:43 -0400

    Add basic functional test for SearchController

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index" do
+    get :index, nil, {current_project_id: Project.first.id}
+    assert_response :success
+    assert_not_nil assigns(:iczn_groups)
+  end
 end

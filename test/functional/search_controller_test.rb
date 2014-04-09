b9dbee96d6ad04a7ef9dd48f43e12eb578af0b7a
Dan Leehr <dan.leehr@nescent.org>
2014-04-09 09:55:47 -0400

    SearchControllerTest is now passing

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 
   test "should get index" do
+    sign_in User.first
     get :index, nil, {current_project_id: Project.first.id}
     assert_response :success
     assert_not_nil assigns(:iczn_groups)
+    sign_out User.first
   end
 end

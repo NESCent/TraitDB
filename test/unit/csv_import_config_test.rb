dbd1c39d190c98018a82923ca8ebbf35f6b2b5cc
Dan Leehr <dan.leehr@nescent.org>
2014-04-11 13:55:30 -0400

    Failing test for csv_import_config

--- test/unit/csv_import_config_test.rb
+++ test/unit/csv_import_config_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CsvImportConfigTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can create with file' do
+    CsvImportConfig.create(:config_file => '')
+  end
 end

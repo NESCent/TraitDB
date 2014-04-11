431d9f407c03f729389bc0c29a5875cc259d0445
Dan Leehr <dan.leehr@nescent.org>
2014-04-11 14:11:29 -0400

    Test for creating a CsvImportConfig w/ yml file

--- test/unit/csv_import_config_test.rb
+++ test/unit/csv_import_config_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class CsvImportConfigTest < ActiveSupport::TestCase
   test 'can create with file' do
-    CsvImportConfig.create(:config_file => '')
+    config_file = File.new(Rails.root + 'lib/traitdb_import/Sample.yml')
+    assert config_file.size > 0, 'config file is empty'
+    csv_import_config = CsvImportConfig.create(:config_file => config_file)
+    assert csv_import_config, 'unable to create sample config'
+    assert_equal 'Sample.yml', csv_import_config.config_file_file_name, 'file name is wrong'
   end
 end

e1f5ff35e54ecfc8a06117762aed4916b38c511e
Dan Leehr <dan.leehr@nescent.org>
2014-04-25 15:07:06 -0400

    Cleanup search-with_trait_sets_test

--- test/integration/search_with_trait_sets_test.rb
+++ test/integration/search_with_trait_sets_test.rb
@@ -8,9 +8,7 @@ class SearchWithTraitSetsTest < ActionDispatch::IntegrationTest
     select projects(:project3).name, :from => 'id'
     click_on 'Select Project'
     visit '/search'
-    print page.html
     select taxa(:animal).name, :from => 'kingdom_0'
-    # Failing here because no javascript
     select trait_sets(:trait_set_demographic).name, :from => 'trait_set_level'
     select 'categorical', :from => 'trait_type'
   end

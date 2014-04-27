87fa6dd8d03567e3be912459161cb80d74775431
Dan Leehr <leehro@gmail.com>
2014-04-26 21:44:26 -0400

    Use find to force capybara to wait for javascript

--- test/integration/search_with_trait_sets_test.rb
+++ test/integration/search_with_trait_sets_test.rb
@@ -4,7 +4,6 @@ class SearchWithTraitSetsTest < ActionDispatch::IntegrationTest
   fixtures :projects, :taxa, :trait_sets, :categorical_traits
   setup do
     Capybara.current_driver = Capybara.javascript_driver
-    Capybara.default_wait_time = 5
   end
   test 'can select trait sets' do
     visit '/'
@@ -17,11 +16,15 @@ class SearchWithTraitSetsTest < ActionDispatch::IntegrationTest
     select trait_sets(:trait_set_reproductive_sub).name, :from => 'level1_0'
     select 'Continuous', :from => 'trait_type_0'
     select 'Categorical', :from => 'trait_type_0'
+    # Find to force capybara to wait for javascript
+    find 'option', :text => categorical_traits(:categorical_trait2).name
     select categorical_traits(:categorical_trait2).name, :from => 'trait_name_0'
     select trait_sets(:trait_set_demographic).name, :from => 'level0_0'
     select trait_sets(:trait_set_demographic_group_size).name, :from => 'level1_0'
     select 'Continuous', :from => 'trait_type_0'
     select 'Categorical', :from => 'trait_type_0'
+    # Find to force capybara to wait for javascript
+    find 'option', :text => categorical_traits(:categorical_trait1).name
     select categorical_traits(:categorical_trait1).name, :from => 'trait_name_0'
   end
 end

87a35d5c6f6b0f0098737c76e856d212898465ba
Dan Leehr <dan.leehr@nescent.org>
2014-05-05 14:32:23 -0400

    Tests and implementation for downloader

--- lib/traitdb_import/downloader.rb
+++ lib/traitdb_import/downloader.rb
@@ -1,27 +1,46 @@
 require 'net/http'
-require 'uri'
 
 # initialize with url
 # return path to downloaded file on local file system
 
 module TraitDB
   class Downloader
-    def initialize(uri=nil)
-      puts uri
-      @file = nil
+    def initialize(source_address=nil,dest_path=nil)
+      @source = source_address
+      @dest = dest_path
     end
 
-    def file
-      if @file.nil?
+    def downloaded_file
+      begin
         download
+      rescue
+        nil
       end
-      return @file
-    end
+
+   end
 
     private
 
-    def download
-      raise 'Not yet implemented'
+    def get_response_with_redirect(uri)
+      r = Net::HTTP.get_response(uri)
+      if r.is_a? Net::HTTPRedirection
+        r = Net::HTTP.get_response(URI.parse(r.header['location']))
+      end
+      r
     end
+
+    def download
+      uri = URI.parse(@source)
+      resp = get_response_with_redirect(uri)
+      if resp.is_a? Net::HTTPSuccess
+        open(@dest, "wb") do |file|
+          file.write(resp.body)
+        end
+        @dest
+      else
+        nil
+      end
+    end
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require 'rails/test_help'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+require 'webmock/test_unit'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -15,6 +16,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include WebMock::API
 end
 
 class ActionDispatch::IntegrationTest

31e7ffc9a103676ebd82ca8a90883db640cc1490
Dan Leehr <dan.leehr@nescent.org>
2014-05-14 10:22:29 -0400

    TestCase for UploadController
    Uses set_project helper

--- test/functional/upload_controller_test.rb
+++ test/functional/upload_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UploadControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can upload and download csv file' do
+    sign_in(User.first)
+    set_project(Project.first)
+
+    get :index
+    puts @response.body
+    assert_template 'upload/index'
+    assert_select 'input', 'dataset_csv_file'
+  end
 end

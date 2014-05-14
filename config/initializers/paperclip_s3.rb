84c26dd79d2d3641d0da29ffdf092b745ea110b5
Dan Leehr <dan.leehr@nescent.org>
2014-05-14 11:24:22 -0400

    Disable S3 in test environment

--- config/initializers/paperclip_s3.rb
+++ config/initializers/paperclip_s3.rb
@@ -1,1 +1,2 @@
-TraitDB::Application.config.use_s3 = !(ENV['TRAITDB_USE_S3'].nil?)
+# Use s3 if specified in environment, but not in test environment
+TraitDB::Application.config.use_s3 = !(ENV['TRAITDB_USE_S3'].nil?) && Rails.env != 'test'

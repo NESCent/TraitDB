169ef0c1bfedc64951bc215dc1da777a44abe884
Dan Leehr <dan.leehr@nescent.org>
2014-07-09 10:37:44 -0400

    Update search_controller_test to use fixtures

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
   include Devise::TestHelpers
+  fixtures :projects
 
   test "should get index" do
     # Only signing in so that is_authenticated_user? doesn't fail
@@ -13,14 +14,20 @@ class SearchControllerTest < ActionController::TestCase
   end
 
   test "should download all" do
-    i = IcznGroup.first
+    project = projects(:project3)
+    assert_not_nil project
+    iczn_group = project.iczn_groups.sorted.first
+    assert_not_nil iczn_group
+    taxon = iczn_group.taxa.sorted.first
+    assert_not_nil taxon
+    assert_includes iczn_group.projects, taxon.project
     params = {download_all: '1',
               select_all_traits: '1',
               include_references: '1',
-              "#{i.name}[0]" => "#{i.id}",
+              "#{iczn_group.name}[0]" => "#{taxon.id}",
               format: :csv
     }
-    post :results, params, {current_project_id: Project.first.id}
+    post :results, params, {current_project_id: project.id}
     assert assigns :results
     assert_response :success
     assert_template 'search/results'

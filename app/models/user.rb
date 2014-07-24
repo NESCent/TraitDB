f23aed1978f0fc65baaac1315d484a254c2babb6
Dan Leehr <dan.leehr@nescent.org>
2014-07-24 16:32:31 -0400

    Remove providers from devise calls
    This was causing open_id provider to fail

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :omniauthable, :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauth_providers => [:openid, :google_oauth2]
-
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :provider, :uid

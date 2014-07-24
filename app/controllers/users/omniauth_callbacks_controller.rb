9aab6818f440297030efe0660486870a60922434
Dan Leehr <dan.leehr@nescent.org>
2014-07-24 16:03:31 -0400

    Tweak markup on Google login
    Google sign-in now working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,10 +10,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
   def google_oauth2
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash.notice = "Signed in Through Google!"
+      flash.notice = "Successfully authenticated with Google"
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes

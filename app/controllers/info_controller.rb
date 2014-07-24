398409e3dfaa108cd9134a35592bf587ef1a516d
Dan Leehr <dan.leehr@nescent.org>
2014-07-24 11:12:42 -0400

    Rename 'about' to 'info'
    About page will be about TraitDB, info will be info about datasets

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,4 @@
-class AboutController < ApplicationController
+class InfoController < ApplicationController
   before_filter :set_project
   def index
   end
--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AboutControllerTest < ActionController::TestCase
+class InfoControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   test 'should redirect to select project' do
     sign_in(User.first)
@@ -8,11 +8,11 @@ class AboutControllerTest < ActionController::TestCase
     get :index
     assert_response :redirect
   end
-  test 'should show about content' do
+  test 'should show info content' do
     sign_in(User.first)
     sign_out(User.first)
     get :index, nil, {current_project_id: Project.first.id}
-    assert_template 'about/index'
+    assert_template 'info/index'
     assert_select 'h3', Project.first.name
   end
 end
